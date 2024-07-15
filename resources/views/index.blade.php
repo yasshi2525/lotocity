ceadbdfcfeb4c6c8e2c28500747095d4ddb1e36a
yasshi2525 <yasshy2@gmail.com>
2024-07-16 03:07:33 +0900

    add GeoMap blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ Config::get('app.locale') }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LotoCity</title>
+</head>
+<body>
+    <div>LotoCity</div>
+    <x-geo-map></x-geo-map>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('index');
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
